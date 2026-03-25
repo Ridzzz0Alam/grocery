201d7d78cd0dd67f61e98cb3b9e70130313299c1
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-03-25 11:47:08 +0100

    added nativewind

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,11 +1,9 @@
-import { Image } from "expo-image";
 import { StyleSheet, Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text>Hello there, And Welcome to the grocery app</Text>
-      <Image source={require("../../assets/images/icon.png")} style={{width:200, height:200}} />
     </View>
   );
 }
