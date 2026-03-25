b8f97e9218b6c7081789afa522448d62706054ba
Ridzzz0Alam <ridwan0gts@gmail.com>
2026-03-25 12:03:05 +0100

    made tailwind work

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,9 @@ import { StyleSheet, Text, View } from "react-native";
 export default function Index() {
   return (
     <View style={styles.container}>
-      <Text>Hello there, And Welcome to the grocery app</Text>
+      <Text className="text-red-500">
+        Hello there, And Welcome to the grocery app
+      </Text>
     </View>
   );
 }
